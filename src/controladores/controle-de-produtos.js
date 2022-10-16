48c08d434739cf732e79daf66b73e7d4b8c91385
Guilherme Ermel <guilherme.ermel@skyinformatica.com.br>
2022-10-16 11:56:15 -0300

    Incluindo middleware de exemplo

--- src/controladores/controle-de-produtos.js
+++ src/controladores/controle-de-produtos.js
@@ -1,17 +1,23 @@
+const express = require('express');
 const criarProduto = require('../servicos/produto/criar-produto');
 const obterProduto = require('../servicos/produto/obter-produto');
 const listarProdutos = require('../servicos/produto/listar-produtos');
 const alterarProduto = require('../servicos/produto/alterar-produto');
 const removerProduto = require('../servicos/produto/remover-produto');
 
+const router = express.Router();
+
+router.use((req, res, next) => {
+    console.log('passou aqui');
+    next();
+})
+
+router.post('/produto', criarProduto);
+router.get('/produto/:codigoDoProduto', obterProduto);
+router.get('/produtos', listarProdutos);
+router.put('/produto', alterarProduto);
+router.delete('/produto', removerProduto);
+
 module.exports = (app) => {
-    app.post('/produto', criarProduto);
-      
-    app.get('/produto/:codigoDoProduto', obterProduto);
-
-    app.get('/produtos', listarProdutos);
-      
-    app.put('/produto', alterarProduto);
-
-    app.delete('/produto', removerProduto);
+    app.use(router); 
 }
