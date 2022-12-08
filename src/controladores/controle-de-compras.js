5b3c61542a84593adf4c78afd4e09c0eb537b916
Guilherme Ermel <guilherme.ermel@skyinformatica.com.br>
2022-12-08 17:55:57 -0300

    Adicionando funcionalidade para obter compras de um usuario

--- src/controladores/controle-de-compras.js
+++ src/controladores/controle-de-compras.js
@@ -5,5 +5,6 @@ const removerCompra = require('../servicos/compra/remover-compra');
 module.exports = (app) => {
     app.post('/compra', comprar);
     app.get('/compras-do-usuario', listarComprasDoUsuario);
+    app.get('/compras-do-usuario/:compradorId', listarComprasDoUsuario);
     app.delete('/compra', removerCompra);
 }
--- src/servicos/compra/listar-compras-do-usuario.js
+++ src/servicos/compra/listar-compras-do-usuario.js
@@ -1,11 +1,15 @@
 const db = require('../../banco-de-dados/banco-de-dados');
 
 module.exports = async (req, res) => {
-    const { compradorId } = req.body;
+    const compradorId = req.params.compradorId || req.body.compradorId;
+
+    if (!compradorId) {
+        res.status(400).send({ error: "Comprador não informado" });
+    }
 
     const compras = await db.compra.findMany({
         where: {
-            compradorId
+            compradorId: Number(compradorId)
         },
         include: {
             itens: true
